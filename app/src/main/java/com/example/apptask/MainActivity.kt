f42de4e907512f87d81eb76478cf7b0fdcf89321
freemake-061 <iizuka@freemake.co.jp>
2024-04-10 14:02:27 +0900

    カンマ付きの数量と±ボタン

--- app/src/main/java/com/example/apptask/MainActivity.kt
+++ app/src/main/java/com/example/apptask/MainActivity.kt
@@ -3,44 +3,96 @@ package com.example.apptask
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.apptask.ui.theme.AppTaskTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppTaskTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+                CreateForm()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CreateForm() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 8.dp)
+    ){
+        var quantity by remember { mutableStateOf(1000) }
+        Text(
+            text = "数量：${"%,d".format(quantity)}",
+            fontSize = 20.sp
+        )
+        Row(
+            modifier = Modifier.align(Alignment.TopEnd),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            Button(
+                onClick = { quantity++ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Green,
+                    contentColor = Color.White,
+                    disabledContainerColor = Color.LightGray,
+                    disabledContentColor = Color.Gray
+                ),
+                modifier = Modifier.size(width = 30.dp, height = 30.dp),
+                shape = RoundedCornerShape(3.dp),
+                enabled = true,
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Text(text = "＋")
+            }
+            Button(
+                onClick = { quantity-- },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red,
+                    contentColor = Color.White,
+                    disabledContainerColor = Color.LightGray,
+                    disabledContentColor = Color.Gray
+                ),
+                modifier = Modifier.size(width = 30.dp, height = 30.dp),
+                shape = RoundedCornerShape(3.dp),
+                enabled = true,
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Text(text = "－")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AppTaskTheme {
-        Greeting("Android")
-    }
+fun QuantityPreview() {
+    CreateForm()
 }
