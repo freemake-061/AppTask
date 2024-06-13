5532fe0992613f4f07fa939468722cbe4cf4b76c
freemake-061 <iizuka@freemake.co.jp>
2024-06-13 09:56:28 +0900

    画像の選択と表示

--- app/src/main/java/com/example/apptask/StockDetailScreen.kt
+++ app/src/main/java/com/example/apptask/StockDetailScreen.kt
@@ -1,7 +1,17 @@
 package com.example.apptask
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -9,9 +19,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 
+@RequiresApi(Build.VERSION_CODES.P)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StockDetailScreen(stock: Stock) {
@@ -38,6 +56,40 @@ fun StockDetailScreen(stock: Stock) {
             Text(text = "clock:${stock.clock}")
             Text(text = "quantity:${stock.quantity}")
             Text(text = "comment:${stock.comment}")
+            ImagePicker()
+
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.P)
+@Composable
+fun ImagePicker() {
+    var imageUri: Uri? by remember { mutableStateOf(null) }
+    val context = LocalContext.current
+    var bitmap: Bitmap? by remember { mutableStateOf(null) }
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
+        if (uri == null) return@rememberLauncherForActivityResult
+        imageUri = uri
+    }
+    Column {
+        imageUri?.let {
+            val source = ImageDecoder.createSource(context.contentResolver, it)
+            bitmap = ImageDecoder.decodeBitmap(source)
+            bitmap?.let { bm ->
+                Image(
+                    bitmap = bm.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+        Button(
+            onClick = {
+                launcher.launch("image/*")
+            }
+        ) {
+            Text(text = stringResource(R.string.form_button_add))
         }
     }
 }
