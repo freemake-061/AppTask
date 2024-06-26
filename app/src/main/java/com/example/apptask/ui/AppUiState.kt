46424cce2f450094bd1f6a1b2843d9fcdaff8724
freemake-061 <iizuka@freemake.co.jp>
2024-06-26 11:46:51 +0900

    viewmodel ファイル用意

---
+++ app/src/main/java/com/example/apptask/ui/AppUiState.kt
@@ -0,0 +1,7 @@
+package com.example.apptask.ui
+
+data class AppUiState(
+    val quantity: Int = 0,
+    val currentTime: String = "",
+    val comment: String = "",
+)
---
+++ app/src/main/java/com/example/apptask/ui/AppViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.apptask.ui
+
+import androidx.lifecycle.ViewModel
+
+class AppViewModel : ViewModel() {
+
+}
