e28dd2d8ca8ae392b79d9508aaaa7cf938e933ea
freemake-061 <iizuka@freemake.co.jp>
2024-07-03 14:07:33 +0900

    追加時間の表示

--- app/src/main/java/com/example/apptask/ui/StockListViewModel.kt
+++ app/src/main/java/com/example/apptask/ui/StockListViewModel.kt
@@ -1,19 +1,26 @@
 package com.example.apptask.ui
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import com.example.apptask.Constants
 
 class StockListViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(StockListUiState())
     val uiState: StateFlow<StockListUiState> = _uiState.asStateFlow()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun addStock(quantity: Int, comment: String) {
-        val newStock = StockUiState(
-            isChecked = false,
-            time = "00:00:00",
+        val formatTime = DateTimeFormatter.ofPattern(Constants.CLOCK_FORMAT)
+        val currentTime = formatTime.format(LocalDateTime.now())
+        val newStock = StockUiState().copy(
+            time = currentTime,
             quantity = quantity,
             comment = comment
         )
