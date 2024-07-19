d05e56ef1e9529b9dc8ea1e83bfcb4e720410157
freemake-061 <iizuka@freemake.co.jp>
2024-07-19 13:37:07 +0900

    リスト部分に項目ごとの画像を表示

--- app/src/main/java/com/example/apptask/ui/StockListScreen.kt
+++ app/src/main/java/com/example/apptask/ui/StockListScreen.kt
@@ -15,7 +15,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.BasicTextField
@@ -52,6 +54,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -61,6 +64,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.Dialog
+import coil.compose.AsyncImage
 import com.example.apptask.Constants
 import com.example.apptask.R
 import com.example.apptask.Route
@@ -173,6 +177,12 @@ fun StockRow(
             Checkbox(
                 checked = stockRowUiState.isChecked,
                 onCheckedChange = { onCheckedChange(index) }
+            )
+            AsyncImage(
+                model = stockRowUiState.stock.uri,
+                contentDescription = stringResource(R.string.list_image_desc),
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.height(20.dp).width(20.dp)
             )
             Text(text = stockRowUiState.stock.time)
             Text(text = "%,d".format(stockRowUiState.stock.quantity))
