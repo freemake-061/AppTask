971b1477c54be102344bb87f152a478d7e7349bd
freemake-061 <iizuka@freemake.co.jp>
2024-08-01 16:11:54 +0900

    動く状態

--- app/src/main/java/com/example/apptask/data/InventoryDatabase.kt
+++ app/src/main/java/com/example/apptask/data/InventoryDatabase.kt
@@ -1,24 +1,9 @@
 package com.example.apptask.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Item::class], version = 1, exportSchema = false)
 abstract class InventoryDatabase : RoomDatabase() {
     abstract fun itemDao(): ItemDao
-
-    companion object {
-        @Volatile
-        private var Instance: InventoryDatabase? = null
-
-        fun getDatabase(context: Context): InventoryDatabase {
-            return Instance ?: synchronized(this) {
-                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
-                    .build()
-                    .also { Instance = it }
-            }
-        }
-    }
 }
